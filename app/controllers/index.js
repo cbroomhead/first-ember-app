2b44652d8707ebd021e46c1b150630d070819f34
cbroomhead <celine.broomhead@gmail.com>
2016-10-04 14:57:37 -0700

    adding a commit with a store.js as well as new functionality adding data and loading data from the store.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,8 +3,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	msg: '',
-	lat: '',
-	lon: '',
 	update: '',
 	ajax: Ember.inject.service(),
 	actions : {
@@ -24,6 +22,31 @@ export default Ember.Controller.extend({
 		getData: function(coordobj){
 			console.log("COORD OBJECT", coordobj);
 
+			var that = this;
+			var store = this.store;
+
+			var newCity = store.createRecord('index', {city: 'Hello worlds'});
+			newCity.save().then(function(){
+				that.send('loadData');
+			});
+		}, 
+		loadData: function(){
+			var that = this;
+				var store = this.store;
+				store.findRecord('index','lt3tk').then(function(data){
+					console.log(data.get('city'));
+				});
+			that.send('changeData');
+		}, 
+		changeData: function(){
+			var that = this;
+			var store = this.store;
+			store.findRecord('index','lt3tk').then(function(data){
+					data.set('city', 'Hello world');
+					data.save().then(function(){
+							console.log(data.get('city'));
+					});
+				});
 		}
 	}
 });
