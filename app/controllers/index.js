ffefb987077b1f1bd6da9c98046d8bd4ad5ea5db
cbroomhead <celine.broomhead@gmail.com>
2016-10-05 17:26:27 -0700

    adding a working app. some issues with populating the model array for the highcharts component. otherwise i was able to load this on my phone and it works properly.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
+import defaultTheme from '../themes/default-theme';
 
 
 export default Ember.Controller.extend({
 	msg: '',
 	errormsg: '',
 	ajax: Ember.inject.service(),
+	piecharts: false,
 	actions : {
 		handleClick () {
 			this.send('getLocation');
@@ -20,7 +22,6 @@ export default Ember.Controller.extend({
 		navigator.geolocation.getCurrentPosition(onSuccess, onError);
 		}, 
 		storeCoords: function(coordobj){ 
-			//the only thing this function really does at this point is store some lat and lon values in the store. store values are not currently used later on.
 			var that = this;
 			var store = this.store;
 			var lat = coordobj.latitude;
@@ -28,7 +29,8 @@ export default Ember.Controller.extend({
 			var location = store.createRecord('index', 
 				{ 
 					latitude: lat,
-					longitude: lon
+					longitude: lon, 
+					chartsdata: []
 				});
 
 			location.save().then(function(newobj){
@@ -36,9 +38,7 @@ export default Ember.Controller.extend({
 			});
 		}, 
 		getBalAuth: function(newobj){
-			console.log("OBJCOORD", newobj._internalModel._data);
 			var that = this;
-			var store = this.store;
 			var lat = newobj._internalModel._data.latitude;
 			var lon = newobj._internalModel._data.longitude;
 			var coorUrl = `https://api.watttime.org/api/v1/balancing_authorities/?loc={"type":"Point","coordinates":[${lon},${lat}]}`;
@@ -46,7 +46,7 @@ export default Ember.Controller.extend({
 		          	url: coorUrl,
 		          	type: 'get',
 		          	headers: {
-            			'Authorization': 'Token 902faab6d04cd6f4dd7cbca4acce9e28de92ea20'
+            			'Authorization': 'Token 91769eaee6281a836ce218415d151634e275b4f1'
           			}
           		}).then(function(data){
           			var name = data[0].name;
@@ -54,43 +54,109 @@ export default Ember.Controller.extend({
           			that.set('msg', `The name of the Balancing authority is ${name} (${abbrev})`);
           			that.send('getChartsData', abbrev);
           		}).catch(function(err){
+          			//error should be handled. need to find a way to cause an error
           			that.set('errormsg','Something bad happened. Please trying again');
           		})
 		},
 		getChartsData: function(ba){
 			var that = this;
-			var store = this.store;
 			$.getJSON({
-		          	url: `https://api.watttime.org/api/v1/datapoints/?ba=${ba}&page=3&page_size=12`,
+		          	url: `https://api.watttime.org/api/v1/datapoints/?ba=${ba}&page=5&page_size=12`,
             		type: 'get'
           		}).then(function(data){
-          			//console.log("DATA", data.results[0].genmix);
-          			var stuff = data.results[0].genmix;
-          			
-          			var newCity = store.createRecord('index', {
-          				chartsdata: stuff
-          			})
-          			console.log(newCity)
-					that.send('displayCharts');
+	console.log("DATA IS HERE", data.results); //this is the array of results, code below  needs to be modified accordingly. 
 
+          			if(data.statusText === "TOO MANY REQUESTS"){
+          				console.log("TOO MANY REQUEST", data.statusText)
+          			}
+          			var args = data.results;
+         			that.send('displayCharts', args)      			
                 }).catch(function(err){
-          			console.log(err);
+          			if(err.status === 429){
+          				var errmesg = err.responseJSON.detail;
+          				that.set('errormsg', `${errmesg}`);
+          			}
+          			else {
+          				that.set('errormsg', 'Something bad happened. Please trying again');
+          			}
           		})
 		},
-		displayCharts: function(){
+		displayCharts: function(args) {
+			console.log("ARGS", args);
 			var that = this;
-			var store = this.store;
-			console.log("INSIDE DISPLAY CHARTS");
-
-			store.findAll('index').then(function(item){
-				console.log(item.getEach('chartsdata'));
-			item.map(function(ele){
-				if(ele != undefined){
-					console.log(ele._internalModel.store._data);
-				}
-			})
-
-			})
+			      var modelarr = [
+				      {
+				        name: 'Other',
+				        data: []
+				      },
+				      {
+				        name: 'Solar',
+				        data: []
+				      },
+				      {
+				        name: 'Wind',
+				        data: []
+				      },
+				      {
+				        name: 'Renewable',
+				        data: []
+				      }
+			    	]
+			    that.set('piecharts', true);
+			    that.set('errormsg', 'We got something'); 
+			    args.map(function(object){
+			    	var blah = object.genmix.map(function(item){
+				    	if (item.fuel === 'other'){
+				    		console.log('other');
+				           modelarr[0].data.push(item.gen_MW);
+				        }
+				        if (item.fuel === 'solar'){
+				        	console.log('solar');
+				           modelarr[1].data.push(item.gen_MW);
+				        }
+				        if (item.fuel === 'wind'){
+				        	console.log('wind');
+				           modelarr[2].data.push(item.gen_MW);
+				        }
+				        if (item.fuel === 'renewable'){
+				        	console.log('renewable');
+				           modelarr[3].data.push(item.gen_MW);
+				        }
+			        })
+				})
+							        console.log(blah);
+		  },
+  		chartOptions: {
+		      chart: {
+		        type: 'line'
+		      },
+		      title: {
+		        text: 'Average Generation Mix'
+		      },
+		      subtitle: {
+		        text: 'Source: WattTime API'
+		      },
+		      xAxis: {
+		        tile : {
+		          text: 'Time'
+		        }
+		      },
+		      yAxis: {
+		        title: {
+		          text: 'Megawatts (MW)'
+		        }
+		      },
+		      tooltip: {
+		        valueSuffix: 'MW'
+		      },
+		      legend: {
+		        layout: 'vertical',
+		        align: 'right',
+		        verticalAlign: 'middle',
+		        borderWidth: 0
+		      }
+		    },
+		model : this.model,
+		theme : defaultTheme
 		}
-	}
 });
