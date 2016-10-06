d960ac1cd6f3611cf8171f1a5e6bce5619788394
cbroomhead <celine.broomhead@gmail.com>
2016-10-06 16:48:50 -0700

    adding working app. lots of visual improvements still need to be done but if need be, the project can be cloned after this point to make it work on android.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,9 +7,24 @@ export default Ember.Controller.extend({
 	errormsg: '',
 	ajax: Ember.inject.service(),
 	piecharts: false,
+	isDisabled: true,
+	spinner: false,
 	actions : {
 		handleClick () {
+			this.set('isDisabled', false);
 			this.send('getLocation');
+			this.send('spinnerTrue');
+		},
+		resetEverything: function() {
+			this.set('msg', '');
+			this.set('errormsg', '');
+			this.set('piecharts', false);
+		},
+		spinnerTrue: function(){
+			console.log("INSIDE THE SPINNER");
+			this.set('spinner', true);
+			var spin = document.getElementById("load");
+			spin.toggle('true');
 		},
 		getLocation: function(){
 			var that = this;
@@ -19,7 +34,7 @@ export default Ember.Controller.extend({
 			function onError(error) {
 				console.log("ERROR", error);
 			}
-		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+			navigator.geolocation.getCurrentPosition(onSuccess, onError);
 		}, 
 		storeCoords: function(coordobj){ 
 			var that = this;
@@ -42,49 +57,42 @@ export default Ember.Controller.extend({
 			var lat = newobj._internalModel._data.latitude;
 			var lon = newobj._internalModel._data.longitude;
 			var coorUrl = `https://api.watttime.org/api/v1/balancing_authorities/?loc={"type":"Point","coordinates":[${lon},${lat}]}`;
-        	$.getJSON({
+        	Ember.$.getJSON({
 		          	url: coorUrl,
 		          	type: 'get',
 		          	headers: {
             			'Authorization': 'Token 91769eaee6281a836ce218415d151634e275b4f1'
           			}
           		}).then(function(data){
+          			that.set('spinner', false);
           			var name = data[0].name;
           			var abbrev = data[0].abbrev;
-          			that.set('msg', `The name of the Balancing authority is ${name} (${abbrev})`);
+          			that.set('msg', `Your Balancing authority is: ${name} (${abbrev})`);
           			that.send('getChartsData', abbrev);
           		}).catch(function(err){
-          			//error should be handled. need to find a way to cause an error
-          			that.set('errormsg','Something bad happened. Please trying again');
-          		})
+          			that.set('errormsg','Somethings bad happened. Please trying again');
+          			console.log(err);
+          		});
 		},
 		getChartsData: function(ba){
 			var that = this;
-			$.getJSON({
+			Ember.$.getJSON({
 		          	url: `https://api.watttime.org/api/v1/datapoints/?ba=${ba}&page=5&page_size=12`,
             		type: 'get'
           		}).then(function(data){
-	console.log("DATA IS HERE", data.results); //this is the array of results, code below  needs to be modified accordingly. 
-
           			if(data.statusText === "TOO MANY REQUESTS"){
-          				console.log("TOO MANY REQUEST", data.statusText)
+          				console.log("TOO MANY REQUEST", data.statusText);
           			}
           			var args = data.results;
-         			that.send('displayCharts', args)      			
+         			that.send('displayCharts', args);   			
                 }).catch(function(err){
-          			if(err.status === 429){
           				var errmesg = err.responseJSON.detail;
-          				that.set('errormsg', `${errmesg}`);
-          			}
-          			else {
-          				that.set('errormsg', 'Something bad happened. Please trying again');
-          			}
-          		})
+          				that.set('errormsg', `${errmesg}`);	
+          		});
 		},
 		displayCharts: function(args) {
-			console.log("ARGS", args);
 			var that = this;
-			      var modelarr = [
+			      var modelArr = [
 				      {
 				        name: 'Other',
 				        data: []
@@ -101,62 +109,59 @@ export default Ember.Controller.extend({
 				        name: 'Renewable',
 				        data: []
 				      }
-			    	]
+			    	];
 			    that.set('piecharts', true);
 			    that.set('errormsg', 'We got something'); 
 			    args.map(function(object){
-			    	var blah = object.genmix.map(function(item){
+			    	object.genmix.map(function(item){
 				    	if (item.fuel === 'other'){
-				    		console.log('other');
-				           modelarr[0].data.push(item.gen_MW);
+				           modelArr[0].data.push(item.gen_MW);
 				        }
 				        if (item.fuel === 'solar'){
-				        	console.log('solar');
-				           modelarr[1].data.push(item.gen_MW);
+				           modelArr[1].data.push(item.gen_MW);
 				        }
 				        if (item.fuel === 'wind'){
-				        	console.log('wind');
-				           modelarr[2].data.push(item.gen_MW);
+				           modelArr[2].data.push(item.gen_MW);
 				        }
 				        if (item.fuel === 'renewable'){
-				        	console.log('renewable');
-				           modelarr[3].data.push(item.gen_MW);
+				           modelArr[3].data.push(item.gen_MW);
 				        }
-			        })
-				})
-							        console.log(blah);
-		  },
-  		chartOptions: {
-		      chart: {
-		        type: 'line'
-		      },
-		      title: {
-		        text: 'Average Generation Mix'
-		      },
-		      subtitle: {
-		        text: 'Source: WattTime API'
-		      },
-		      xAxis: {
-		        tile : {
-		          text: 'Time'
-		        }
-		      },
-		      yAxis: {
-		        title: {
-		          text: 'Megawatts (MW)'
-		        }
-		      },
-		      tooltip: {
-		        valueSuffix: 'MW'
-		      },
-		      legend: {
-		        layout: 'vertical',
-		        align: 'right',
-		        verticalAlign: 'middle',
-		        borderWidth: 0
-		      }
-		    },
-		model : this.model,
-		theme : defaultTheme
-		}
+			        });
+				});
+			console.log("MODELARR", modelArr);
+			that.set('chartData', modelArr);
+		},
+	},
+	chartOptions: {
+			      chart: {
+			        type: 'line'
+			      },
+			      title: {
+			        text: 'Average Generation Mix'
+			      },
+			      subtitle: {
+			        text: 'Source: WattTime API'
+			      },
+			      xAxis: {
+			        tile : {
+			          text: 'Time'
+			        }
+			      },
+			      yAxis: {
+			        title: {
+			          text: 'Megawatts (MW)'
+			        }
+			      },
+			      tooltip: {
+			        valueSuffix: 'MW'
+			      },
+			      legend: {
+			        layout: 'vertical',
+			        align: 'right',
+			        verticalAlign: 'middle',
+			        borderWidth: 0
+			      }
+	},  		
+	model : this.chartData,
+	theme : defaultTheme
 });
